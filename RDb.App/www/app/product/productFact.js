53bd2d2da5c87eff486696f6cd98b107faff12d4
R Salisbury <rsalisbury@terpsys.com>
2015-10-19 11:32:17 -0400

    Updates

---
+++ RDb.App/www/app/product/productFact.js
@@ -0,0 +1,34 @@
+﻿(function() {
+    var productFact = function($q, sqlSvc, iDbSvc, apiSvc, queryableFact) {
+        var dataToDto = function(scan, lookup) {
+            return {
+                Upc: scan.Upc,
+                Name: lookup.itemname,
+                Alias: lookup.alias,
+                Description: lookup.description,
+                AveragePrice: lookup.avg_price,
+                DateScanned: scan.DateTime
+            }
+        }
+        var dtoselector = function (item) {
+            return {
+                RId: item.RId,
+                Upc: item.upc,
+                Name: item.itemname,
+                Alias: item.alias,
+                Description: item.description,
+                AveragePrice: item.avg_price,
+                DateScanned: item.datetime
+            }
+        }
+        var toDtos = function (results) {
+            return results.select(dtoselector);
+        }
+        return {
+            toDto: dataToDto,
+            toDtos: toDtos
+        }
+    }
+
+    angular.module("RDb").factory("productFact", ["$q", "sqlSvc", "indexedDbSvc", "apiSvc", "queryableFact", productFact]);
+})();
